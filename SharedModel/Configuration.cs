8afb05ec7da1a668f8be61b1e4eb6d7ed79ab1e6
Thomas Pedersen <s103665@student.dtu.dk>
2015-04-16 11:28:45 +0200

    Added Configuration class.

---
+++ SharedModel/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModel
+{
+    public class Configuration : IMessage
+    {
+        public List<string> ReportToNodes { get; set; }
+        public int ListeningPort { get; set; }
+    }
+}
