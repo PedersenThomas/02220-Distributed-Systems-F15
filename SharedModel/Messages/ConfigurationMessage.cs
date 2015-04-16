736485363575ee855593d95a3c41f77a51bc537c
Thomas Pedersen <s103665@student.dtu.dk>
2015-04-16 16:07:26 +0200

    Moved Messages and made Display fetch Configuration

---
+++ SharedModel/Messages/ConfigurationMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModel.Messages
+{
+    public class RequestConfigurationMessage : IMessage
+    {
+        public string id { get; set; }
+    }
+
+    public class ResponseConfigurationMessage : IMessage
+    {
+        public Configuration Configuration { get; set; }
+    }
+}
