8576be153173f4daa4cb02fbeed59c0c626c2651
Kim Knutzen <kimknutzen@gmail.com>
2015-04-16 16:15:36 +0200

    Added DeviceAddress and wrote configuration of the system

--- Datastore/Program.cs
+++ Datastore/Program.cs
@@ -10,6 +10,22 @@ namespace Datastore
 {
     class Program
     {
+
+        static Dictionary<string, Configuration> configurationSetup = new Dictionary<string, Configuration>
+        {
+            {"display01", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",12345)},9000)},
+            {"display02", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9000)},9001)},
+            {"display03", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9000)},9002)},
+                                                                                               
+            {"sensor_01", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9001)},9100)},
+            {"sensor_02", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9001)},9101)},
+            {"sensor_03", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9001)},9102)},
+                                                                                               
+            {"sensor_04", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9002)},9103)},
+            {"sensor_05", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9002)},9104)},
+            {"sensor_06", new Configuration(new List<DeviceAddress>{new DeviceAddress("localhost",9002)},9105)},
+        };
+        
         static void Main(string[] args)
         {
             var dataStore = new ConcurrentDictionary<string, Status>();
---
+++ SharedModel/DeviceAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModel
+{
+    public class DeviceAddress
+    {
+        public string IP;
+        public int Port;
+
+        public DeviceAddress(string ip, int port)
+        {
+            this.IP = ip;
+            this.Port = port;
+        }
+    }
+}
